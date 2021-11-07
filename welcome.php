89c3e152d7ad84056d5211f01d16cd9295878420
Qimae <kartelbwoy@gmail.com>
2021-11-07 20:36:15 +0300

    Welcome page Update

---
+++ welcome.php
@@ -0,0 +1,42 @@
+<?php
+
+session_start();
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>
+
+  </title>
+
+  <style>
+    .image {
+      background-repeat: no-repeat;
+      position: relative;
+      min-height: 100vh;
+      background-size: cover;
+
+    }
+
+    .text {
+      color: #11101d;
+      font-size: 15px;
+      font-weight: 500;
+      margin: 18px;
+      text-align: center;
+    }
+  </style>
+</head>
+
+<body class="image" style="background-image: url('bg 1.jpg');height: 500px;width: 100%;"><br>
+  <div class="text">
+    <h1>Welcome, Authentication Done!!</h1>
+  </div>
+
+</body>
+
+
+
+</html>
